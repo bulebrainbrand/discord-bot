012cd80d8235cb8ea67edd5dcf2c55a8da0578bf
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-02-20 18:51:59 +0900

    add listen sys

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 require("dotenv").config()
+
+const express = require("express")
+const app = express()
+const port = process.env.PORT
+
+app.get("/",(req,res) => {
+    res.status(200).send('okokokok')
+})
+
+app.listen(port,() => {
+    console.log("listening")
+})
+
+
 const { Client,GatewayIntentBits,Events} = require("discord.js")
 
 const client = new Client({
