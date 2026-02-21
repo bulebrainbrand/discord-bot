36519a7735117936daa64ac09c26a1c7447c1be8
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-02-21 22:20:22 +0900

    split bloxd fetch logic

--- commands/bloxdstatus.js
+++ commands/bloxdstatus.js
@@ -1,19 +1,6 @@
 const {SlashCommandBuilder,EmbedBuilder} = require("discord.js")
 
-let cacheStatus = null
-let cacheTime = null
-
-const getStatus = async () => {
-    if (cacheStatus && cacheTime && cacheTime && cacheTime + 1000*60*5 > Date.now()) {
-        return cacheStatus
-    }
-    const start = performance.now()
-    const response = await fetch("https://bloxd.io")
-    const end = performance.now()
-    cacheStatus = {code:response.status,text:response.statusText,ok:response.ok,time:end-start}
-    cacheTime = Date.now()
-    return cacheStatus
-}
+const {getStatus} = require("getbloxdstatus")
 
 module.exports = {
     data:new SlashCommandBuilder()
